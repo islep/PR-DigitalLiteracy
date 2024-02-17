687bc6288a70f465ebab3c19f5b0b754ca87ffb4
Florian268 <florian@reihl-schaaf.net>
2024-02-17 09:38:45 -0500

    Made the subtopic selection looks similar. Still need to refactor it to make it have the same sizing parameter as the video.

--- src/components/Video/SubtopicSelection/index.jsx
+++ src/components/Video/SubtopicSelection/index.jsx
@@ -12,16 +12,16 @@ export function SubtopicSelector({ subtopics, dataFromSubtopicSelector }) {
 	return (
 
 		<div className="w-full flex items-center justify-center pb-36">
-			<div className="flex flex-wrap justify-center items-stretch gap-4 md:gap-14 max-w-screen-xl">
+			<div className="grid grid-cols-2 gap-4 md:gap-9 max-w-screen-xl">
 				{subtopics.map((subtopic, index) => (
 					<div
-						className="w-full md:w-[calc(33.3333%-3rem)] bg-white rounded-xl cursor-pointer shadow-1 hover:shadow-12 hover:scale-105 transition-all duration-150"
+						className="w-full bg-white rounded-xl cursor-pointer shadow-1 hover:shadow-12 hover:scale-105 transition-all duration-150"
 						onClick={() => handleSubtopicSelection(subtopic)}
 						key={index}
 					>
 						<div className="flex-col items-stretch justify-center w-full p-8">
 							<div className="border-b border-gray-200">
-								<img src={placeholderImage} alt={subtopic} className="h-38 mx-auto my-4 p-4" />
+								<img src={placeholderImage} alt={subtopic} className=" h-full w-full my-4 p-4" />
 							</div>
 							<h5 className="text-xl font-bold py-3" style={{ textAlign: 'center' }}>{subtopic}</h5>
 						</div>
