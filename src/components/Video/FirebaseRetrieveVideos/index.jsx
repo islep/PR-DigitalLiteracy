bf6d4e9417d5b11bc6f71db65f790f3efa1fe6ca
Florian268 <florian@reihl-schaaf.net>
2024-02-21 22:45:43 -0500

    change item.url to item.url due to duplicate videos in different pages

--- src/components/Video/FirebaseRetrieveVideos/index.jsx
+++ src/components/Video/FirebaseRetrieveVideos/index.jsx
@@ -33,7 +33,8 @@ export function FirebaseRetrieveVideos({ dataFromFirebase, dataFromIntro }) {
 		console.log('useEffect 3');
 		console.log('dataFromFirebase type is ', typeof dataFromFirebase);
 		if (Array.isArray(dataFromFirebase)) {
-			const transformedData = dataFromFirebase.map((item) => ({
+			const transformedData = dataFromFirebase.map((item) => (
+				{
 				category: item.category,
 				messages: item.messages,
 				operating_system: item.operating_system,
@@ -41,7 +42,7 @@ export function FirebaseRetrieveVideos({ dataFromFirebase, dataFromIntro }) {
 				tags: item.tags,
 				url: item.url,
 				subtopic: item.subtopic,
-				key: item.url,
+				key: item.key,
 			}));
 			setosValue(transformedData);
 		} else {
