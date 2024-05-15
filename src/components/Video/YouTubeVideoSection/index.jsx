26d872a964a65c5e9966b403eb01a4a5c8363633
Florian268 <frr20@pitt.edu>
2024-05-14 22:39:13 -0400

    Replaced Iframe with react video player for easier implmentation, implemented popup for stopTimes, fixed ESLINT issues with YoutubeVideoSection

--- src/components/Video/YouTubeVideoSection/index.jsx
+++ src/components/Video/YouTubeVideoSection/index.jsx
@@ -1,101 +1,106 @@
-import React, { useState, useEffect } from 'react';
-import '../../../Layouts/Main/YouTubeVideo/youtubeVideo.css';
-import { Box, Grid, CircularProgress } from '@mui/material';
-import { TagsInput } from 'react-tag-input-component';
-import YoutubeEmbed from '../../../Layouts/Main/YouTubeVideo/youtubeVideoEmbed';
+import React, { useState, useEffect} from 'react';
+import PropTypes from 'prop-types';
+import ReactPlayer from 'react-player/youtube';
+import { Box } from '@mui/material';
+import Popup from '../Popup/Popup';
+
 import { Colors } from '../../../constants/Colors';
 import './youtubeVideoSection.css';
-import { set } from 'lodash';
-import PropTypes from 'prop-types';
+import '../../../Layouts/Main/YouTubeVideo/youtubeVideo.css';
 
-export function YouTubeVideoSection({ osvalue, subtopicValue, tags, appliedFilterTags }) {
+export default function YouTubeVideoSection({ osvalue, subtopicValue, tags, appliedFilterTags }) {
 	// this is just a parameter to hide videos without a subtopic during testing
 	const showSubtopicUndefinedVideos = true;
 
 	const [videos, setVideos] = useState(osvalue || []);
 	console.log('Initial osvalue:', osvalue);
-	const videoIdRegex =
-		/(?:(?:https?:\/\/)?(?:www\.)?)?youtu(?:\.be\/|be.com\/(?:watch\?(?:.*&)?v=|(?:embed|v)\/))([\w'-]+)/i;
+
+	const [playerRefs, setPlayerRefs] = useState([]);
+	const [popup, setPopup] = useState(null);
 
 	useEffect(() => {
-		if (window.YT && window.YT.Player && Array.isArray(videos)) {
-			videos.forEach((video, index) => {
-				const match = video.url.match(videoIdRegex);
-				const videoId = match ? match[1] : null;
-				new window.YT.Player(`player-${index}`, {
-					videoId,
-				});
-			});
-		}
-	}, [videos]);
-
-	// console.log(videos);
-	useEffect(() => {
-		// Load the IFrame Player API code asynchronously.
-		if (!window.YT) {
-			const tag = document.createElement('script');
-			tag.src = 'https://www.youtube.com/iframe_api';
-			const firstScriptTag = document.getElementsByTagName('script')[0];
-			firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-		}
-
-		window.onYouTubeIframeAPIReady = () => {
-			videos.forEach((video, index) => {
-				const match = video.url.match(videoIdRegex);
-				const videoId = match ? match[1] : null;
-				new window.YT.Player(`player-${index}`, {
-					videoId,
-				});
-			});
-		};
-
-		setVideos(osvalue);
-	}, [osvalue]);
-
-	useEffect(() => {
-		// retrieve all videos
-		const videos = osvalue.filter(
-			(video) => (videoTags, inputTags) => inputTags.every((inputTag) => videoTags.includes(inputTag)),
-		);
-
-		// filter by content type
-		const categoryVideos = videos.filter((video) => appliedFilterTags.includes(video.category));
-
-		// filter by operating system
-		const osVideos = categoryVideos.filter(
-			(video) =>
+		// Combined filter operation
+		const filteredVideos = osvalue.filter((video) => {
+			// Filter by content type
+			const contentTypeMatch = appliedFilterTags.includes(video.category);
+	
+			// Filter by operating system
+			const osMatch =
 				video.operating_system.includes('All') ||
-				(!Array.isArray(video.operating_system) &&
-					videos.filter((video) => appliedFilterTags.includes(video.operating_system))) ||
-				(Array.isArray(video.operating_system) && video.operating_system.some((os) => appliedFilterTags.includes(os))),
-		);
-
-		// filter by subtopic
-		const subtopicVideos = osVideos.filter(
-			(video) =>
+				appliedFilterTags.includes(video.operating_system) ||
+				(Array.isArray(video.operating_system) && video.operating_system.some(os => appliedFilterTags.includes(os)));
+	
+			// Filter by subtopic
+			const subtopicMatch =
 				(showSubtopicUndefinedVideos && !video.subtopic) ||
-				(video.subtopic && (subtopicValue.length == 0 || subtopicValue == video.subtopic)),
-		);
-
-		// filter by tags
-		const tagVideos = subtopicVideos.filter(
-			(video) =>
+				(video.subtopic && (subtopicValue.length === 0 || subtopicValue === video.subtopic));
+	
+			// Filter by tags
+			const tagsMatch =
 				tags.length === 0 ||
-				(!Array.isArray(video.tags) && videos.filter((video) => tags.includes(video.tags))) ||
-				(Array.isArray(video.tags) && video.tags.some((tag) => tags.includes(tag))),
-		);
-
-		setVideos(tagVideos);
+				(Array.isArray(video.tags) && video.tags.some(tag => tags.includes(tag)));
+	
+			return contentTypeMatch && osMatch && subtopicMatch && tagsMatch;
+		});
+	
+		setPlayerRefs(filteredVideos.map(_ => React.createRef()));
+		setVideos(filteredVideos);
+	
 	}, [tags, osvalue, subtopicValue, appliedFilterTags]);
+
+	const handleProgress = (progress, video, playerRef) => {
+		const currentTime = Math.floor(progress.playedSeconds);
+		if (video.stopTimes && video.stopTimes.includes(currentTime)) {
+			const playerState = playerRef.current.getInternalPlayer().getPlayerState();
+			if (playerState === 1) {
+				playerRef.current.getInternalPlayer().pauseVideo();
+				if (document.fullscreenElement) {
+					document.exitFullscreen();
+				}
+				const currentTimeIndex = video.stopTimes.indexOf(currentTime);
+				const text = currentTimeIndex !== -1 ? video.messages[currentTimeIndex] : ''; 
+				setPopup({
+					text,
+					visible: true,
+					playerRef
+				});
+			}
+		}
+	};
 
 	if (videos && videos.length > 0) {
 		return (
 			<div className="h-full w-full grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-6 max-w-screen-xl">
-				{videos.map((video, index) => (
-					<div key={video.key}>
-						<div className="h-72 w-full" id={`player-${index}`} />
+				{videos.map((video,index) => (
+					<div key={video.key} className="h-72 w-full">
+						<ReactPlayer
+							key={video.key}
+							ref={playerRefs[index]}
+							className='react-player'
+							url = {video.url}
+							width='100%'
+							height='100%'
+							onProgress={(progress) => handleProgress(progress, video, playerRefs[index])}
+							config={{
+								youtube: {
+									playerVars: {         
+										controls: 1,
+										showinfo: 1
+									}
+								}
+							}}
+						/>
 					</div>
-				))}
+				))},
+				{popup && popup.visible && (
+					<Popup 
+						text={popup.text} 
+						handleClose={() => {
+							setPopup(null);
+							popup.playerRef.current.getInternalPlayer().playVideo();
+						}} 
+					/>
+				)}
 			</div>
 		);
 	}
@@ -122,10 +127,8 @@ export function YouTubeVideoSection({ osvalue, subtopicValue, tags, appliedFilte
 }
 
 YouTubeVideoSection.propTypes = {
-	osvalue: PropTypes.array.isRequired,
+	osvalue: PropTypes.arrayOf(PropTypes.Obj).isRequired,
 	subtopicValue: PropTypes.string.isRequired,
-	tags: PropTypes.array,
-	appliedFilterTags: PropTypes.array,
+	tags: PropTypes.arrayOf(PropTypes.string).isRequired,
+	appliedFilterTags:PropTypes.arrayOf(PropTypes.string).isRequired,
 };
-
-export default YouTubeVideoSection;
